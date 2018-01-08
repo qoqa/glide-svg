374a11a586023746efab8bb1c105f524e5f8ea62
Kevin Robatel <kevin.robatel@qoqa.com>
2018-01-08 21:58:02 +0100

    Refacto

--- library/src/main/java/ch/qoqa/glide/svg/SvgDrawableTranscoder.kt
+++ library/src/main/java/ch/qoqa/glide/svg/SvgDrawableTranscoder.kt
@@ -1,11 +1,8 @@
 package ch.qoqa.glide.svg
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
-import android.graphics.drawable.PictureDrawable
 import com.bumptech.glide.load.Options
 import com.bumptech.glide.load.engine.Resource
 import com.bumptech.glide.load.resource.SimpleResource
@@ -13,19 +10,10 @@ import com.bumptech.glide.load.resource.transcode.ResourceTranscoder
 import com.caverock.androidsvg.SVG
 
 class SvgDrawableTranscoder(private val context: Context) : ResourceTranscoder<SVG, Drawable> {
+    private val bitmapTranscoder = SvgBitmapTranscoder()
+
     override fun transcode(toTranscode: Resource<SVG>, options: Options): Resource<Drawable> {
-        val svg = toTranscode.get()
-        val width = svg.documentWidth.toInt().takeIf { it > 0 }
-                ?: (svg.documentViewBox.right - svg.documentViewBox.left).toInt()
-        val height = svg.documentHeight.toInt().takeIf { it > 0 }
-                ?: (svg.documentViewBox.bottom - svg.documentViewBox.top).toInt()
-
-        val picture = svg.renderToPicture(width, height)
-        val drawable = PictureDrawable(picture)
-
-        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
-        canvas.drawPicture(drawable.picture)
+        val bitmap = bitmapTranscoder.transcode(toTranscode, options).get()
 
         return SimpleResource(BitmapDrawable(context.resources, bitmap))
     }
